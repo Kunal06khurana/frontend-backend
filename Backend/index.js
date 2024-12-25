4e52e02694e1b4c7130347f1bbe6aede14644e34
Kunal06khurana <iit2023501@iiita.ac.in>
2024-12-26 02:16:03 +0530

    Cors Policy

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,9 @@ import cors from 'cors'
 
 const app=express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'frontend-backend-blond.vercel.app'], // Add all allowed frontend URLs
+    origin: ['http://localhost:5173', 'https://frontend-backend-blond.vercel.app'], // Ensure the correct URL with protocol
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true, // Enable cookies and authentication headers if needed
+    credentials: true // Support for cookies and authentication headers
   }));
 
 
